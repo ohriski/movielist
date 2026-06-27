0e062fa4f2612363df15f92967fa94ef3c3f3c08
ohriski <ihordrevnytskiy@gmail.com>
2026-06-27 19:10:17 +0300

    f

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useList } from "../context/ListContext";
+import { useState, useEffect } from "react";
 
 const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
